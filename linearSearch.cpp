8e92c67a16e39c7ad923ac03640cad7e263880e1
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-04 16:12:24 +0700

    menambahkan perintah untuk memasukkan pilihan untuk lanjut mencari atau tidak, yang akan diuji sebagai kondisi pada looping do ... while pada prosedur LinearSearch

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -48,6 +48,8 @@ void LinearSearch() {
             cout << "\n" << item << "not found in the array\n";
         cout << "\nNumber of comparisons: " << ctr << endl;
 
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
     }
 }
 
