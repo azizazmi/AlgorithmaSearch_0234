5e3f1f541c3bcefb25b232422b89bfb6390ee012
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-04 16:22:45 +0700

    menambahkan perintah untuk menampilkan elemen-elemen array pada prosedur display

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -55,7 +55,10 @@ void LinearSearch() {
 }
 
 void display() {
-
+    cout << "Array elements are: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
 int main()
