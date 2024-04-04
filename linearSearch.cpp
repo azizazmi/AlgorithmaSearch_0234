1d55c68ab75bf0a7e6dd103ac242d1e7845520ea
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-04 16:28:09 +0700

    memperbaiki line 48 agar lebih rapi hasilnya saat di-run

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -45,7 +45,7 @@ void LinearSearch() {
             i++;  //step 4
         }
         if (i == n)  //step 5
-            cout << "\n" << item << "not found in the array\n";
+            cout << "\n" << item << " not found in the array\n";
         cout << "\nNumber of comparisons: " << ctr << endl;
 
         cout << "\nContinue search (y/n): ";
