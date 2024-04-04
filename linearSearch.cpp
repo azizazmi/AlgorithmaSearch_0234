ed2ce4fe6eee49fb6b8369f8600b5deccb08e805
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-04 15:50:04 +0700

    mendeklarasikan i = 0 pada prosedur LinearSearch/step 2

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -33,6 +33,9 @@ void LinearSearch() {
         //accept the number to be searched
         cout << "\nEnter the element you want to search: ";   //step 1
         cin >> item;
+
+        ctr = 0;
+        i = 0;   //step 2
     }
 }
 
